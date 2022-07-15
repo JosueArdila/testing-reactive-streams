51cdc1d0026ae91d8650166c7ae0014b31f2bfaa
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-14 23:50:33 -0500

    first commit

---
+++ src/test/java/com/example/reactive/ServicioTest.java
@@ -0,0 +1,131 @@
+package com.example.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+import java.time.Duration;
+
+@ExtendWith(MockitoExtension.class)
+class ServicioTest {
+    @InjectMocks
+    Servicio servicio;
+
+    final TestPublisher<String> testPublisher = TestPublisher.create();
+
+    @Test
+    void testMono() {
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .verifyComplete();
+    }
+
+    @Test
+    void testVarios() {
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .expectNext("Maria")
+                .expectNext("Jesus")
+                .expectNext("Carmen")
+                .verifyComplete();
+    }
+
+    @Test
+    void testVariosLento() {
+        Flux<String> uno = servicio.buscarTodosLento();
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1))
+                .verifyComplete();
+    }
+
+    @Test
+    void testTodosFiltro1() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .create(source)
+                .expectNext("JOHN")
+                .expectNextMatches(name -> name.startsWith("MA"))
+                .expectNext("CLOE", "CATE")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testTodosFiltro2() {
+        Flux<String> source = servicio.buscarTodosFiltro();
+        StepVerifier
+                .create(source)
+                .expectNextCount(4)
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
+                        throwable.getMessage().equals("Mensaje de Error")
+                ).verify();
+    }
+
+    @Test
+    void testTime() {
+        StepVerifier.withVirtualTime(() -> Flux.interval(Duration.ofSeconds(1)).take(2))
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(1))
+                .expectNext(0L)
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void testEmitter() {
+        Flux<Integer> source = Flux.<Integer>create(emitter -> {
+            emitter.next(1);
+            emitter.next(2);
+            emitter.next(3);
+            emitter.complete();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            emitter.next(4);
+        }).filter(number -> number % 2 == 0);
+
+        StepVerifier.create(source)
+                .expectNext(2)
+                .expectComplete()
+                .verifyThenAssertThat()
+                .hasDropped(4)
+                .tookLessThan(Duration.ofMillis(1050));
+    }
+
+    @Test
+    void testUpperCase() {
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
+    }
+
+    @Test
+    void testComportamientoInesperado() {
+        TestPublisher
+                .createNoncompliant(TestPublisher.Violation.ALLOW_NULL)
+                .emit("1", "2", null, "3");
+    }
+
+
+
+
+}
